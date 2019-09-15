f9f3cb42e3c212c66f2b48859fbbb7db4d6d78a2
schiughi <schiughi@gmail.com>
2019-09-15 13:14:44 +0900

    :+1: enable server sent event

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -3,8 +3,17 @@ import ReactDOM from 'react-dom';
 
 const Index: FC = () => <div>Hello React!</div>;
 
-fetch('/api/').then(response => {
-  console.log(response.json());
+const es = new EventSource('/api/stream');
+es.addEventListener('open', () => {
+  console.log('opend!');
+});
+
+es.addEventListener('error', e => {
+  console.error('es error', e);
+});
+
+es.addEventListener('date', date => {
+  console.log(date);
 });
 
 ReactDOM.render(<Index />, document.getElementById('root'));
--- src/server/index.js
+++ src/server/index.js
@@ -1,15 +1,27 @@
 import express from 'express';
 import path from 'path';
+import SSE from 'express-sse';
 
 const app = express();
 
 app.use(express.static(path.join('./', 'dist')));
 
+const sse = new SSE('handshaked!', { isSerialized: false });
+
 app.get('/api', (req, res) => {
   res.send({ api: 'test' });
 });
 
-app.get('*', function(req, res) {
+app.get('/api/stream', (req, res) => {
+  sse.init(req, res);
+  setInterval(() => {
+    // eslint-disable-next-line @typescript-eslint/ban-ts-ignore
+    // @ts-ignore
+    sse.send(new Date().toLocaleString(), 'date');
+  }, 1000);
+});
+
+app.get('*', (req, res) => {
   res.sendFile(path.join('./', 'dist', 'index.html'));
 });
 
